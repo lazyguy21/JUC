0d9d8090f37a3f579466ca01e702d25ad7b5765e
lazyguy21 <15882041972@sina.cn>
2016-07-14 15:51:49 +0800

    Why Runnable cannot throw CheckedException

--- src/main/java/lazyguy/yyf/api/ExceptionInRunnableTest.java
+++ src/main/java/lazyguy/yyf/api/ExceptionInRunnableTest.java
@@ -4,4 +4,21 @@ package lazyguy.yyf.api;
  * Created by tobi on 16-7-14.
  */
 public class ExceptionInRunnableTest {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            long id = Thread.currentThread().getId();
+            System.out.println(id);
+            int i = 3 / 0;
+
+        });
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                long id = Thread.currentThread().getId();
+                System.out.println(id);
+                System.out.println(e);
+            }
+        });
+        thread.start();
+    }
 }
