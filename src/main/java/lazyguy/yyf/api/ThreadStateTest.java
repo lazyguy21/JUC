ad9338137dee1382744fab67a15d5cfadc25f44b
lazyguy21 <15882041972@sina.cn>
2016-07-14 15:52:12 +0800

    Sleep is Timed_waiting state

--- src/main/java/lazyguy/yyf/api/ThreadStateTest.java
+++ src/main/java/lazyguy/yyf/api/ThreadStateTest.java
@@ -1,7 +1,24 @@
 package lazyguy.yyf.api;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by tobi on 16-7-14.
  */
 public class ThreadStateTest {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(()->{
+            System.out.println(Thread.currentThread().getState());
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        Thread.State state = thread.getState();
+        System.out.println("state when sleep :"+state);
+    }
 }
