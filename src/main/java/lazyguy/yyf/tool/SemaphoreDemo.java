5862a0daf04e5c50785ce222b7f98ca5a2e1a6cf
lazyguy21 <15882041972@sina.cn>
2016-07-19 10:02:12 +0800

    ssemaphore

--- src/main/java/lazyguy/yyf/tool/SemaphoreDemo.java
+++ src/main/java/lazyguy/yyf/tool/SemaphoreDemo.java
@@ -1,7 +1,37 @@
 package lazyguy.yyf.tool;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by tobi on 16-7-19.
  */
 public class SemaphoreDemo {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(4);
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                if (semaphore.tryAcquire()) {
+                    try {
+                        System.out.println("I got a permit ,do something!");
+                        TimeUnit.SECONDS.sleep(5);
+                        semaphore.release();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    System.out.println("I do not get permit,quit~");
+                }
+            }).start();
+        }
+        Runnable runnable = () -> {
+            try {
+                semaphore.acquire();
+                System.out.println("不管等多久，俺最后还是得到了ｐｅｒｍｉｔ");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(runnable).start();
+    }
 }
